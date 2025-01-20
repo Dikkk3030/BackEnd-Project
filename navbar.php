a47f87325ab597e38fade72e8d5d21cd127c09c8
manalah92 <okapramn092@gmail.com>
2025-01-20 19:59:20 +0800

    Navbar update

--- navbar.php
+++ navbar.php
@@ -1,37 +1,35 @@
 <?php
-session_start();
+    session_start();
 ?>
 <nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Membatik</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+     <div class="container-fluid">
+         <a class="navbar-brand" href="#">Membatik</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
+      <div class="collapse navbar-collapse" id="navbarNav">
+             <ul class="navbar-nav ms-auto ">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php">Home</a>
+                    <a class="nav-link" href="index.php">Home </a>
                 </li>
+
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Kategori</a>
+                    <a class="nav-link" href="product.php">Produk</a>
                 </li>
+                
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Produk</a>
-                </li>
-                <li class="nav-item">
-                    <?php
+                <?php
                     if (isset($_SESSION['loginuser']) && $_SESSION['loginuser'] == true) {
                         echo '<a class="nav-link" href="accountinfo.php">Info Akun</a>';
                     } else {
                         echo '<a class="nav-link" href="login.php">Login</a>';
                     }
-                    ?>
-                </li>
-            </ul>
-        </div>
-    </div>
+                ?>
+                </li> 
+                <script src="bootstrap/js/bootstrap.bundle.js"></script>
+                <script src="/fontawesome/js/all.min.js"></script>
+</body>
+  </div>
+  </div>
 </nav>
-
-<script src="bootstrap/js/bootstrap.bundle.js"></script>
-<script src="fontawesome/js/all.min.js"></script>
